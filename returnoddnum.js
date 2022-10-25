ade6eb07419e8aa27a20b8e001a96004ea7708de
Alain Makoso (Leny) <106040804+lenynzita@users.noreply.github.com>
2022-10-25 18:09:14 -0400

    add new file

---
+++ returnoddnum.js
@@ -0,0 +1,8 @@
+//Create a function that will return an odd number 
+
+function oddNumber(n) {
+    return Math.floor(n/2)
+}
+//We create a function first, 
+//we apply the Math.floor is a function that round any number to the smallest
+//so, n/2 will always return an odd number.
