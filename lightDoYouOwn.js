c2acd555eaf13915aa76188fcf65a2a8f0c669c9
Alain Makoso (Leny) <106040804+lenynzita@users.noreply.github.com>
2022-11-18 09:34:56 -0500

    new commit

---
+++ lightDoYouOwn.js
@@ -0,0 +1,9 @@
+//Write a code where ZAch has 18 light and anyone else 0.
+
+function lightDoYouOwn(name){
+    if(name === "Zach" || name === "zach"){
+        return 18
+    }else{
+        return 0;
+    }
+}
